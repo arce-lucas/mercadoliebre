debe532ac9cc3b59a2058661eaa3cd51ddb25a03
arce-lucas <arce_lucas@hotmail.com>
2023-05-08 22:50:28 -0300

    subiendo ML

--- Desktop/Mercado/app.js
+++ Desktop/Mercado/app.js
@@ -1,14 +1,14 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname,'./public');
+app.use(express.static('public'));
+/* const PORT=process.env.PORT || 3000; */
 
-const app = express ();
-const PORT=process.env.PORT || 3000;
 
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./views/home.'));
 })
 
 app.get('/formlogin', (req, res) => {
@@ -18,7 +18,7 @@ app.get('/formlogin', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/register.html'));
 })
-
-app.listen (PORT, () => {
+app.listen(3000,() => {console.log("Servidor escuchando puerto 3000")});
+/* app.listen (3000, () => {
     console.log('Servidor corriendo en el puerto'+PORT);
-});
+}); */
